aef2443094a43c585a4ad511a9bcdb9361ef8e85
hbc3869 <hbc3869@gmail.com>
2025-01-16 12:51:32 +0900

    Add Svg & Setting Main Header

---
+++ src/components/footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div className="bg-gray-900 px-4 py-16 text-white"></div>;
+};
+
+export default Footer;
---
+++ src/components/header.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import MenuIcon from "assets/svg/menu";
+import { useState } from "react";
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="sticky top-0 z-50 bg-gray-900/90 p-4 text-white backdrop-blur-md">
+      <div className="mx-auto flex items-center justify-between md:container">
+        <div className="flex items-center space-x-4 text-xl font-bold">
+          <a
+            href="/"
+            className="inline-flex items-center justify-center whitespace-nowrap text-lg h-10 px-4 py-2"
+          >
+            0's Life
+          </a>
+        </div>
+        <div className="hidden items-center space-x-4 md:flex">
+          <a
+            href="/"
+            className="inline-flex items-center justify-center whitespace-nowrap text-sm h-10 px-4 py-2"
+          >
+            오늘
+          </a>
+          <a
+            href="/"
+            className="inline-flex items-center justify-center whitespace-nowrap text-sm h-10 px-4 py-2"
+          >
+            전체
+          </a>
+          <a
+            href="/"
+            className="inline-flex items-center justify-center whitespace-nowrap text-sm h-10 px-4 py-2"
+          >
+            스트리머별
+          </a>
+          <a
+            href="/"
+            className="inline-flex items-center justify-center whitespace-nowrap text-sm h-10 px-4 py-2"
+          >
+            공식방송
+          </a>
+        </div>
+        <button className="md:hidden" onClick={handleMenu}>
+          <MenuIcon />
+        </button>
+      </div>
+      <div
+        className={`space-y-4 md:hidden transition-all duration-500 ease-in-out overflow-hidden ${
+          isOpen ? "h-52" : "h-0"
+        }`}
+      >
+        <a
+          href="/"
+          className="inline-flex items-center justify-center whitespace-nowrap text-sm h-10 px-4 py-2 w-full"
+        >
+          오늘
+        </a>
+        <a
+          href="/"
+          className="inline-flex items-center justify-center whitespace-nowrap text-sm h-10 px-4 py-2 w-full"
+        >
+          전체
+        </a>
+        <a
+          href="/"
+          className="inline-flex items-center justify-center whitespace-nowrap text-sm h-10 px-4 py-2 w-full"
+        >
+          스트리머별
+        </a>
+        <a
+          href="/"
+          className="inline-flex items-center justify-center whitespace-nowrap text-sm h-10 px-4 py-2 w-full"
+        >
+          공식방송
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
