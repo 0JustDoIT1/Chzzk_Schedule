ee02c2413f949363034613151d4c4dcf8ee16837
hbc3869 <0justdoit1@gmail.com>
2025-05-23 19:23:20 +0900

    Edit Swagger

--- servers/back-end/src/schedule/schedule.controller.ts
+++ servers/back-end/src/schedule/schedule.controller.ts
@@ -3,17 +3,22 @@ import { ScheduleService } from './schedule.service';
 import { CreateScheduleDto } from './dto/create-schedule.dto';
 import { Schedule } from 'src/schemas/schedule.schema';
 import { IDateSchedule } from 'src/lib/types/schedule.type';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Schedule')
 @Controller(ApiPath.SCHEDULE)
 export class ScheduleController {
   constructor(private readonly scheduleService: ScheduleService) {}
 
+  @ApiOperation({ summary: '스케줄 추가 API' })
   @Post(ApiPath.SCHEDULE_ADD)
+  @ApiBody({ description: '스케줄 데이터', type: CreateScheduleDto })
   async create(@Body() scheduleData: CreateScheduleDto): Promise<Schedule> {
     console.log('테스트', scheduleData);
     return await this.scheduleService.create(scheduleData);
   }
 
+  @ApiOperation({ summary: 'date에 맞는 스케줄 리스트 반환 API' })
   @Get(ApiPath.SCHEDULE_BY_DATE)
   async getScheduleListByDate(
     @Param('date') date: string,
--- servers/back-end/src/streamer/streamer.controller.ts
+++ servers/back-end/src/streamer/streamer.controller.ts
@@ -2,30 +2,37 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { StreamerService } from './streamer.service';
 import { CreateStreamerDto } from './dto/create-streamer.dto';
 import { Streamer } from 'src/schemas/streamer.schema';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Streamer')
 @Controller(ApiPath.STREAMER)
 export class StreamerController {
   constructor(private readonly streamerService: StreamerService) {}
 
+  @ApiOperation({ summary: '스트리머 추가 API' })
+  @Post(ApiPath.STREAMER_ADD)
+  @ApiBody({ description: '스트리머 데이터', type: CreateStreamerDto })
+  async create(@Body() streamerData: CreateStreamerDto): Promise<Streamer> {
+    return await this.streamerService.create(streamerData);
+  }
+
+  @ApiOperation({ summary: '전체 스트리머 리스트 반환 API' })
   @Get()
   async getAllStreamerList(): Promise<Streamer[]> {
     return await this.streamerService.getAllStreamerList();
   }
 
+  @ApiOperation({ summary: 'Object Id에 해당하는 스트리머 반환 API' })
   @Get(ApiPath.STREAMER_BY_ID)
   async getStreamerById(@Param('id') id: string): Promise<Streamer | null> {
     return await this.streamerService.getStreamerById(id);
   }
 
+  @ApiOperation({ summary: 'name에 해당하는 스트리머 반환 API' })
   @Get(ApiPath.STREAMER_BY_NAME)
   async getStreamerByName(
     @Param('name') name: string,
   ): Promise<Streamer | null> {
     return await this.streamerService.getStreamerByName(name);
-  }
-
-  @Post(ApiPath.STREAMER_ADD)
-  async create(@Body() streamerData: CreateStreamerDto): Promise<Streamer> {
-    return await this.streamerService.create(streamerData);
   }
 }
